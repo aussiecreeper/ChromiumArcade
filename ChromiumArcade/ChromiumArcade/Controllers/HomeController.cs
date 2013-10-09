dfd0f502a3b693eb2df140ac48c83ca37dd575e3
aussiecreeper <pokemonband1@gmail.com>
2013-10-09 20:03:51 +1100

    Interface Changes
    Changes to the homepage.

--- ChromiumArcade/ChromiumArcade/Controllers/HomeController.cs
+++ ChromiumArcade/ChromiumArcade/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace ChromiumArcade.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "This is just awesome.";
 
             return View();
         }
